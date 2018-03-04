99e33ac370c36a347235055203faac0799e2524e
Jun <wangzhen_tju@126.com>
2018-03-04 14:11:07 +0800

    使用jstl前端遍历list

--- src/main/java/controller/Welcome.java
+++ src/main/java/controller/Welcome.java
@@ -1,25 +1,34 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import model.Mobile;
+
+
 @Controller
 public class Welcome  {
-
-	@RequestMapping(path="/hello",method = RequestMethod.GET)
-	public String helloUser(@RequestParam(required=false) String uname, Model model){
-		System.out.println("收到了客户端的请求");
-		model.addAttribute("uname", uname);
-		return "welcome";
-	}
 	
-	@RequestMapping(path="/user",method = RequestMethod.GET)
-	public String user(@RequestParam(required=false) String uname, Model model){
-		System.out.println("收到了客户端的请求");
-		model.addAttribute("uname", uname);
+	private List<Mobile> mobileList = new ArrayList<Mobile>();
+	
+	public Welcome() {
+		mobileList.add(new Mobile("华为手机", 4000));
+		mobileList.add(new Mobile("小米手机", 1000));
+		mobileList.add(new Mobile("苹果手机", 7000));
+		mobileList.add(new Mobile("oppo手机", 2000));
+		mobileList.add(new Mobile("vivo手机", 3000));
+		mobileList.add(new Mobile("锤子手机", 1500));
+	}
+
+	@RequestMapping(path="/mobiles",method = RequestMethod.GET)
+	public String user(Model model){
+		model.addAttribute("mList", this.mobileList);
 		return "welcome";
 	}
 }
--- src/main/webapp/WEB-INF/jsp/welcome.jsp
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -4,9 +4,18 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<%@ taglib prefix="c" 
+           uri="http://java.sun.com/jsp/jstl/core" %>
 <title>Insert title here</title>
 </head>
 <body>
-hello ${uname} !!!!!
+<table border="1" >
+<tr><th>名称</th><th>价格</th></tr>
+<tr>
+<c:forEach items="${mList }" var="mobile">
+	<tr><td>${mobile.tradeMark }</td><td>${mobile.price }</td></tr>
+</c:forEach>
+</tr>
+</table>
 </body>
 </html>
