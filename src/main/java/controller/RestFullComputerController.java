a758d0944fe4b59327ed51053563ca3580b11dca
Jun <wangzhen_tju@126.com>
2018-03-18 17:36:54 +0800

    集成mybatis

--- src/main/java/controller/RestFullComputerController.java
+++ src/main/java/controller/RestFullComputerController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import MyException.PCFormException;
 import model.AjaxModel;
 import model.Computer;
+import pojo.ComputerPojo;
 import service.ComputerService;
 
 @RestController
@@ -44,5 +47,12 @@ public class RestFullComputerController {
 		AjaxModel am = new AjaxModel(200, "computer added successfully!");
 		
 		return am;
+	}
+	
+	@RequestMapping(path="/getComPojos",method=RequestMethod.GET)
+	@ResponseBody
+	public List<ComputerPojo> getComputerPojos(){
+		
+		return computerService.getAllComputerPojo();
 	}
 }
