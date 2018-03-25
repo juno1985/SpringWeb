f38bad3777438d86d3c18f21a2621e6efd6346ab
Jun <wangzhen_tju@126.com>
2018-03-25 13:31:24 +0800

    使用@Param

--- src/main/java/mapper/ComputerMapper.java
+++ src/main/java/mapper/ComputerMapper.java
@@ -1,10 +1,16 @@
 package mapper;
 
 import java.util.List;
-
+import org.apache.ibatis.annotations.Param;
 import pojo.ComputerPojo;
 
 public interface ComputerMapper {
 
 	public List<ComputerPojo> getAllComputers();
+	
+	//按id进行查询
+	public ComputerPojo getById(@Param(value="computerId")Integer cid);
+	
+	public ComputerPojo getByIdAndTradeMark(@Param(value="cid")Integer cid
+				, @Param(value="tradeMark")String tradeMark);
 }
--- src/main/test/ComputerPackage/ComputerTest.java
+++ src/main/test/ComputerPackage/ComputerTest.java
@@ -1,13 +1,13 @@
 package ComputerPackage;
 
 import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import mapper.ComputerMapper;
 import pojo.ComputerPojo;
 import service.ComputerService;
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -19,9 +19,25 @@ public class ComputerTest {
 	@Autowired
 	private ComputerService computerService;
 	
+	@Autowired ComputerMapper computerMapper;
+	
 	@Test
 	public void testGetAllComputerPojo(){
 		List<ComputerPojo> comList = computerService.getAllComputerPojo();
 		System.out.println(comList);
+	}
+	
+	@Test
+	public void testGetById(){
+		
+		ComputerPojo comPojo = computerMapper.getById(1);
+		
+		System.out.println(comPojo);
+	}
+	
+	@Test
+	public void testGetByIdAndTradeMark(){
+		ComputerPojo comPojo = computerMapper.getByIdAndTradeMark(1, "hp%");
+		System.out.println(comPojo);
 	}
 }
