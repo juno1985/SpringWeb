9e9bc210b0e4bff5395eedb57a9c0ded882c8d48
Jun <wangzhen_tju@126.com>
2018-04-15 16:09:32 +0800

    MyBatis逆向工程及查询演示

--- src/main/test/ComputerPackage/ComputerTest.java
+++ src/main/test/ComputerPackage/ComputerTest.java
@@ -1,8 +1,20 @@
 package ComputerPackage;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.softcits.pc.mapper.MbgCommentMapper;
+import org.softcits.pc.mapper.MbgComputerMapper;
+import org.softcits.pc.model.MbgComment;
+import org.softcits.pc.model.MbgCommentExample;
+import org.softcits.pc.model.MbgComputer;
+import org.softcits.pc.model.MbgComputerExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -22,6 +34,12 @@ public class ComputerTest {
 	private ComputerService computerService;
 	
 	@Autowired ComputerMapper computerMapper;
+	
+	@Autowired
+	MbgComputerMapper mbgComputerMapper; 
+	
+	@Autowired
+	MbgCommentMapper mbgCommentMapper;
 	
 	@Test
 	public void testGetAllComputerPojo(){
@@ -68,14 +86,7 @@ public class ComputerTest {
 		ComputerPojo computerPojo = new ComputerPojo();
 		computerPojo.setTradeMark("axus电脑");
 		computerPojo.setPrice(8888.99f);
-		//作业: 完成电脑添加功能,要求完成JSP/controller/service
-		/**提示
-		 * JSP:要有form传tradeMark/price/pic
-		 * Controller:要得到来自JSP的参数
-		 * Service:要调用ComputerMapper的insertComputer方法
-		 * 最好使用异步,即页面jsp能收到添加成功或失败的返回
-		 * 尝试引入BOOTSTRAP
-		 */
+		
 		computerPojo.setPic("xxxxxxxx.png");
 		computerMapper.insertComputer(computerPojo);
 	}
@@ -121,6 +132,74 @@ public class ComputerTest {
 	public void getComputerCommentsById() {
 		List<ComputerComment> CCs = computerMapper.getComputerCommentById(1);
 		System.out.println(CCs);
+	}
+	
+	//mybatis逆向工程
+	@Test
+	public void testMbg() throws Exception {
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		File configFile = new File(".\\src\\main\\resources\\mgb.xml");
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+				callback, warnings);
+		myBatisGenerator.generate(null);
+	}
+	
+	@Test
+	public void testSelectMyBatisGenerator() {
+		MbgComputerExample computerExa = new MbgComputerExample();
+		MbgComputerExample.Criteria computerCri = computerExa.createCriteria();
+		//查詢id是1的
+		computerCri.andIdEqualTo(1);
+		
+		List<MbgComputer> mbgComList = mbgComputerMapper.selectByExample(computerExa);
+		for(MbgComputer mbgC : mbgComList)
+		{
+			System.out.println(mbgC.getTrademark());
+		}
+	
+	}
+	
+	@Test
+	public void testSelectMutiByIdMyBatisGenerator() {
+		MbgComputerExample computerExa = new MbgComputerExample();
+		MbgComputerExample.Criteria computerCri = computerExa.createCriteria();
+		//查詢價格在1000~5000之間
+		computerCri.andPriceBetween(1000.00f, 5000.00f);
+		
+		List<MbgComputer> mbgComList = mbgComputerMapper.selectByExample(computerExa);
+		for(MbgComputer mbgC : mbgComList)
+		{
+			System.out.println(mbgC.getTrademark() + ' ' + mbgC.getPrice());
+		}
+	}
+	
+	@Test
+	public void testSelectMutiTableMyBatisGenerator() {
+		MbgComputerExample computerExa = new MbgComputerExample();
+		MbgComputerExample.Criteria computerCri = computerExa.createCriteria();
+		//查詢id是1的
+		computerCri.andIdEqualTo(1);
+		
+		List<MbgComputer> mbgComList = mbgComputerMapper.selectByExample(computerExa);
+		
+		MbgComputer mgbComputer = mbgComList.get(0);
+		
+		Integer cid = mgbComputer.getId();
+		
+		MbgCommentExample commentExa = new MbgCommentExample();
+		MbgCommentExample.Criteria commentCri = commentExa.createCriteria();
+		
+		commentCri.andFkEqualTo(1);
+		
+		List<MbgComment> mbgCommentList = mbgCommentMapper.selectByExample(commentExa);
+		
+		for(MbgComment mbgC : mbgCommentList) {
+			System.out.println(mbgC.getPcComment());
+		}
 	}
 
 }
